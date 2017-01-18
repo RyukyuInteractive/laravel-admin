70842fbf28559e68afb787ed0a22f6688f762cfe
Tatsuto Maetsu <m.tatsuto@gmail.com>
2017-01-18 17:29:05 +0900

    Create lang.php

---
+++ lang/ja/lang.php
@@ -0,0 +1,76 @@
+<?php
+
+return [
+    'online'        => 'オンライン',
+    'login'         => 'ログイン',
+    'logout'        => 'ログアウト',
+    'name'          => '名前',
+    'username'      => 'ユーザー名',
+    'password'      => 'パスワード',
+    'remember_me'   => 'Remember me',
+
+    'list'          => 'リスト',
+    'new'           => '新規',
+    'create'        => '作成',
+    'delete'        => '削除',
+    'remove'        => '取り除く',
+    'edit'          => '編集',
+    'view'          => 'View',
+    'reset'         => 'リセット',
+    'export'        => '出力',
+    'batch_delete'  => '一括削除',
+    'save'          => '保存',
+    'refresh'       => '更新',
+    'order'         => 'Order',
+    'expand'        => 'Expand',
+    'collapse'      => 'Collapse',
+    'filter'        => 'Filter',
+    'close'         => '閉じる',
+    'show'          => '表示',
+    'entries'       => '件',
+
+    'action'           => '操作',
+    'title'            => 'タイトル',
+    'description'      => '概要',
+    'back'             => '戻る',
+    'back_to_list'     => 'リストへ戻る',
+    'submit'           => '決定',
+    'menu'             => 'メニュー',
+    'input'            => '入力',
+    'succeeded'        => '成功',
+    'failed'           => '失敗',
+    'delete_confirm'   => '本当に削除してもよろしいでしょうか ?',
+    'delete_succeeded' => '削除完了！',
+    'delete_failed'    => '削除失敗 !',
+    'update_succeeded' => '更新完了！',
+    'save_succeeded'   => '保存完了 !',
+    'refresh_succeeded'=> '更新完了 !',
+
+    'choose'        => '選択',
+    'choose_file'   => 'ファイル選択',
+    'choose_image'  => '画像選択',
+
+    'more'          => 'もっとみる',
+    'deny'          => '権限なし',
+
+    'administrator' => '管理者',
+    'roles'         => '役割',
+    'permissions'   => '権限',
+    'slug'          => 'Slug',
+
+    'created_at'    => '作成日時',
+    'updated_at'    => '更新日時',
+
+    'alert'         => 'アラート',
+
+    'parent_id'     => '親',
+    'icon'          => 'アイコン',
+    'uri'           => 'URI',
+
+    'operation_log'       => '操作履歴',
+    'parent_select_error' => 'Parent select error',
+
+    'pagination'    => [
+        'range'  => 'Showing :first to :last of :total entries',
+    ],
+];
