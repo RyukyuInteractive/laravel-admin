40101e5b86c5ca7d68b9a040b026303e7cc288e4
Tatsuto Maetsu <m.tatsuto@gmail.com>
2017-02-01 16:20:19 +0900

    Update display form

--- views/form/display.blade.php
+++ views/form/display.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group">
     <label class="col-sm-2 control-label">{{$label}}</label>
     <div class="col-sm-6">
-        <div class="box box-solid box-default no-margin">
+        <div class="no-margin">
             <!-- /.box-header -->
             <div class="box-body">
                 {!! $value !!}&nbsp;
